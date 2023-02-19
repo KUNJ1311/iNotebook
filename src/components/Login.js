619dc5deb651ed4fccef5ba6e9470d2bd1dbfd5f
Kunj1311 <74526794+KUNJ1311@users.noreply.github.com>
2023-02-19 09:58:07 +0530

    fix

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,6 @@ import { React, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-	const key = process.env.WEB_LINK;
 	const [credentials, setCredentials] = useState({ email: "", password: "" });
 	let navigate = useNavigate();
 	const handleSubmit = async (e) => {
